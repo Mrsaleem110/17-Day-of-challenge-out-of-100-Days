8146396ef0ebd6e375d3072d5f5b34ee8ed2e9a9
muhammad_saleem <160356353+Mrsaleem110@users.noreply.github.com>
2024-05-15 11:21:27 +0500

    adding 17 day of challenge

---
+++ Question 50/main.js
@@ -0,0 +1,4 @@
+// *Question 50:* Multiline Template Literals: Use template literals to create a multiline string that describes your ideal day. Include at least three different activities.
+// *Explain & TIP:* Template literals support multiline strings, making it easier to create readable text blocks that incorporate variables and expressions.
+var myidealday = "My ideal day would involve:\n1.I getup early in the morning i take breakfast and after taking it ,I starting coding.\n2.After zohar salah i do lunch and after it i do again coding.\n3.After Asar salah i like to take tea and after it i do again coding as same  like night rutine.\n";
+console.log(myidealday);
---
+++ Question 50/main.ts
@@ -0,0 +1,10 @@
+
+// *Question 50:* Multiline Template Literals: Use template literals to create a multiline string that describes your ideal day. Include at least three different activities.
+
+// *Explain & TIP:* Template literals support multiline strings, making it easier to create readable text blocks that incorporate variables and expressions.
+let myidealday=`My ideal day would involve:
+1.I getup early in the morning i take breakfast and after taking it ,I starting coding.
+2.After zohar salah i do lunch and after it i do again coding.
+3.After Asar salah i like to take tea and after it i do again coding as same  like night rutine.
+`;
+console.log(myidealday)
